efa3aaa6843780a8ea7b27bc1c8b07ca3299ff7b
Priscylla Silva <mss.priscylla@gmail.com>
2014-02-17 22:51:13 -0300

    Update VideoFacadeImpl

--- src/main/java/com/tips/easyoutubeapi/business/facade/impl/VideoFacadeImpl.java
+++ src/main/java/com/tips/easyoutubeapi/business/facade/impl/VideoFacadeImpl.java
@@ -11,8 +11,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.google.api.client.util.DateTime;
 import com.tips.easyoutubeapi.business.facade.VideoFacade;
 import com.tips.easyoutubeapi.business.service.VideoService;
+import com.tips.easyoutubeapi.dto.CaptionDTO;
 import com.tips.easyoutubeapi.dto.SimpleVideoDTO;
+import com.tips.easyoutubeapi.dto.StatisticsDTO;
+import com.tips.easyoutubeapi.dto.SubtitleDTO;
+import com.tips.easyoutubeapi.entity.Caption;
 import com.tips.easyoutubeapi.entity.SimpleVideo;
+import com.tips.easyoutubeapi.entity.Statistics;
+import com.tips.easyoutubeapi.entity.Subtitle;
 
 @Service
 @Transactional
@@ -32,7 +38,7 @@ public class VideoFacadeImpl implements VideoFacade {
 		List<SimpleVideoDTO> simpleVideoDTOList = new ArrayList<SimpleVideoDTO>();
 		
 		for (SimpleVideo simpleVideo : simpleVideoList) {
-			
+			simpleVideoDTOList.add(simpleVideoToSimpleVideoDTO(simpleVideo));
 		}
 		
 		return simpleVideoDTOList;
@@ -56,15 +62,52 @@ public class VideoFacadeImpl implements VideoFacade {
 		simpleVideoDTO.setChannelId(simpleVideo.getChannelId());
 		simpleVideoDTO.setChannelTitle(simpleVideo.getChannelTitle());
 		simpleVideoDTO.setDuration(simpleVideo.getDuration());
-		simpleVideoDTO.setCaption(caption);
+		simpleVideoDTO.setCaption(captionToCaptionDTO(simpleVideo.getCaption()));
 		simpleVideoDTO.setPublishedAt(DateTimeToDate(simpleVideo.getPublishedAt()));
-		simpleVideoDTO.setStatistics(simpleVideo.getStatistics());
+		simpleVideoDTO.setStatistics(StatisticsToStatisticsDTO(simpleVideo.getStatistics()));
 		simpleVideoDTO.setThumbnail(simpleVideo.getThumbnail());
 		
 		return simpleVideoDTO;
 	}
 
+	private CaptionDTO captionToCaptionDTO(Caption caption) {
+		CaptionDTO captionDTO;
+		if(caption==null)
+			return null;
+		else
+			captionDTO = new CaptionDTO();
+			captionDTO.setSubtitles(subtitleListToSubtitleDTOList(caption.getSubtitles()));
+			return captionDTO;
+	}
+
+	private List<SubtitleDTO> subtitleListToSubtitleDTOList(List<Subtitle> subtitles) {
+		List<SubtitleDTO> subtitleDTOList = new ArrayList<SubtitleDTO>();
+		for (Subtitle subtitle : subtitles) {
+			subtitleDTOList.add(subtitleToSubtitleDTO(subtitle));
+		}
+		return subtitleDTOList;
+	}
+
+	private SubtitleDTO subtitleToSubtitleDTO(Subtitle subtitle) {
+		SubtitleDTO subtitleDTO = new SubtitleDTO();
+		subtitleDTO.setText(subtitle.getText());
+		subtitleDTO.setLanguage(subtitle.getLanguage().getName());
+		return subtitleDTO;
+	}
+
+	private StatisticsDTO StatisticsToStatisticsDTO(Statistics statistics) {
+		StatisticsDTO statisticsDTO = new StatisticsDTO();
+		statisticsDTO.setCommentCount(statistics.getCommentCount());
+		statisticsDTO.setDislikeCount(statistics.getDislikeCount());
+		statisticsDTO.setFavoriteCount(statistics.getFavoriteCount());
+		statisticsDTO.setLikeCount(statistics.getLikeCount());
+		statisticsDTO.setViewCount(statistics.getViewCount());
+		return statisticsDTO;
+	}
+
 	private Date DateTimeToDate(DateTime originalTime) {
+		if(originalTime==null)
+			return null;
 		return new java.util.Date(originalTime.getValue());
 	}
 
